4707e1c2b8aae9a1f0a09efcc9b346e9833d4807
Shivam973664 <singhshivam1705@gmail.com>
2025-11-10 11:39:43 +0530

    Capgemini Coding round

---
+++ src/abc/test/capgemini/AddTwoNumbersExample.java
@@ -0,0 +1,12 @@
+package abc.test.capgemini;
+
+@FunctionalInterface
+public interface AddTwoNumbersExample {
+	
+	public void addTwoNumber(Integer a,Integer b);
+	
+	static void exampleStatic() {
+		System.out.println("Static method class");
+	}
+	
+}
---
+++ src/abc/test/capgemini/Test.java
@@ -0,0 +1,33 @@
+package abc.test.capgemini;
+
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+//		AddTwoNumbersExample add= (a,b)-> {
+//			System.out.println(a+b);
+//		};
+//		add.addTwoNumber(5, 6);
+//		AddTwoNumbersExample.exampleStatic();
+		
+		
+//		/write a program to reverse a number
+		Integer num =1234;
+		int ans=0;
+		while(num>0) {
+			int digit = num%10;
+			ans= ans*10+digit;
+			num/=10;
+		}
+		System.out.println(ans);
+		
+		
+	}
+}
+//Q= predict the output?
+//		//listOfIntegers = {1 to 10}
+//		 
+//		Stream s = listOfIntegers.stream().filter(i -> i%2 == 0);
+//		Sysout()
+//		List l1=s.collect(Collector.toList());
